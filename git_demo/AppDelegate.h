07fe34518ff462a366b35c9e819068d653ddf8ce
ljl <1304622859@qq.com>
2016-04-12 09:38:45 +0800

    first commit

---
+++ git_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git_demo
+//
+//  Created by 罗精灵 on 16/4/5.
+//  Copyright © 2016年 luojingling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git_demo
+//
+//  Created by 罗精灵 on 16/4/5.
+//  Copyright © 2016年 luojingling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
